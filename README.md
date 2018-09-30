4f7ca5f91528f41d4ec59c9ea960e14613040166
Carl <carlnegro@gmail.com>
2018-09-29 20:57:45 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# test-package
+
+This is an example package with modules and submodules that import correctly.
+
+## Installation
+
+git clone http://github.com/carl9384/test-package
+cd test-package
+pip install .
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='test-package',
+      version='0.1',
+      description='Example package demonstrating imports',
+      url='http://github.com/carl9384/test-package',
+      author='Carl',
+      author_email='carlnegro@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
